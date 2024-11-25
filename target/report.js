7ca19aa47a77329f38618a863189de930d95d0f6
Karti <karthicksubramanian29@gmail.com>
2024-11-25 14:46:36 +0530

    second commit

---
+++ target/report.js
@@ -0,0 +1,377 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/TaskNov19/featureFiles/bookMedicine.feature");
+formatter.feature({
+  "name": "Booking medicine functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "Launch browser and website of Pharmeasy",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Launch the browser and website",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HOOKS.hooks.launch_the_browser_and_website()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate Booking medicine functionality with valid data",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Select the medicine tab TC1",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TaskNov19.TC001.select_the_medicine_tab_TC1()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter valid medicine and click search button TC1",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC001.enter_valid_medicine_and_click_search_button_TC1()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Choose particular product from product list TC1",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC001.choose_particular_product_from_product_list_TC1()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click AddToCart button TC1",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC001.click_AddToCart_button_TC1()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter quantity of product and click ViewCart button TC1",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC001.enter_quantity_of_product_and_click_ViewCart_button_TC1()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Check the product name and quantity in Cart page TC1",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaskNov19.TC001.check_the_product_name_and_quantity_in_Cart_page_TC1()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "Launch browser and website of Pharmeasy",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Launch the browser and website",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HOOKS.hooks.launch_the_browser_and_website()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate Booking medicine functionality with invalid data",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Select the medicine tab TC2",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TaskNov19.TC002.select_the_medicine_tab_TC2()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter valid medicine and click search button TC2",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC002.enter_valid_medicine_and_click_search_button_TC2()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Check the error message showing on the screen TC2",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaskNov19.TC002.check_the_error_message_showing_on_the_screen_TC2()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "Launch browser and website of Pharmeasy",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Launch the browser and website",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HOOKS.hooks.launch_the_browser_and_website()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate the booking Lab Test with valid data",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Select the Lab Test tab TC3",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TaskNov19.TC003.select_the_Lab_Test_tab_TC3()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Select any Certified Lab partner TC3",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC003.select_any_Certified_Lab_partner_TC3()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Choose the Test type TC3",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC003.choose_the_Test_type_TC3()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the number of patients TC3",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC003.enter_the_number_of_patients_TC3()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click ViewCart button TC3",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC003.click_ViewCart_button_TC3()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Check the Test type and Patient count in Cart page TC3",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaskNov19.TC003.check_the_Test_type_and_Patient_count_in_Cart_page_TC3()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "Launch browser and website of Pharmeasy",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Launch the browser and website",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HOOKS.hooks.launch_the_browser_and_website()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate the booking Lab Test with invalid data",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Select the Lab Test tab TC4",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TaskNov19.TC004.select_the_Lab_Test_tab_TC4()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Select any Certified Lab partner TC4",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC004.select_any_Certified_Lab_partner_TC4()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click ViewCart button with out filling Test type and patient count TC4",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC004.click_ViewCart_button_with_out_filling_Test_type_and_patient_count_TC4()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Check the error message displayed in the page TC4",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaskNov19.TC004.check_the_error_message_displayed_in_the_page_TC4()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.background({
+  "name": "Launch browser and website of Pharmeasy",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Launch the browser and website",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "HOOKS.hooks.launch_the_browser_and_website()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Validate Choose your location functionality",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Select the Pincode button TC5",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.select_the_Pincode_button_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the Pincode,Address and click Check button TC5",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.enter_the_Pincode_Address_and_click_Check_button_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Select the medicine tab TC5",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.select_the_medicine_tab_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter valid medicine and click search button TC5",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.enter_valid_medicine_and_click_search_button_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Choose particular product from product list TC5",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.choose_particular_product_from_product_list_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click AddToCart button TC5",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.click_AddToCart_button_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter quantity of product and click ViewCart button TC5",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.enter_quantity_of_product_and_click_ViewCart_button_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Check the product name and quantity in Cart page TC5",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TaskNov19.TC005.check_the_product_name_and_quantity_in_Cart_page_TC5()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
